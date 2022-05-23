1012191f221a0d3bfacf08ee9b145725e915801f
Sajad Rahimi <75030204+SajadRahimi1@users.noreply.github.com>
2022-05-23 15:00:48 +0430

    Update download.py

--- download.py
+++ download.py
@@ -2,13 +2,13 @@ import requests
 import multi
 import os
 
-url = "https://telegramfiles.s3.ir-thr-at1.arvanstorage.com/fan.png"
+url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.0.1-stable.tar.xz"
 
 response = requests.get(url)
 
-open("files/fan2.png","wb").write(response.content)
+open("files/flutter_linux_3.0.1-stable.tar.xz","wb").write(response.content)
 
-file_rel_path: str = os.path.join('files', "fan2.png")
+file_rel_path: str = os.path.join('files', "flutter_linux_3.0.1-stable.tar.xz")
 # file_abs_path: str = os.path.join(base_directory, file_rel_path)
 print(file_rel_path)
-multi.upload_file(file_rel_path, 'telegramfiles', "fan2.png")
+multi.upload_file(file_rel_path, 'telegramfiles', "flutter_linux_3.0.1-stable.tar.xz")
